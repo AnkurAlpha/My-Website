21f99fc971d106a18718bf312b61762d75c33b44
Ankur Kumar <ankuralpha404@gmail.com>
2024-12-10 15:43:43 +0530

    add some codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            colors: {
+                'primary':"rgb(100,0,0)"
+            },
+            fontFamily : {
+                'display':["Cedarville Cursive","cursive"], // will be downlaoded if not present
+                'Inter': [ "Edu AU VIC WA NT Pre", "cursive" ],
+                'body': ["Robot","Sans-serif"]
+            }
+        },
+    },
+    plugins: [],
 }
 
